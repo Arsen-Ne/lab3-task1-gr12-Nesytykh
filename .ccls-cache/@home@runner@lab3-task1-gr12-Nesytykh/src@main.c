a3cdb57663e29bf9e3714ea3e1dd2da5424e4d42
Arsen-Ne <90570235+Arsen-Ne@users.noreply.github.com>
2023-05-28 17:42:40 +0000

    var 13 main

--- .ccls-cache/@home@runner@lab3-task1-gr12-Nesytykh/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr12-Nesytykh/src@main.c
@@ -1,7 +1,14 @@
-/* main.c */
+#include <stdbool.h>
 #include <stdio.h>
 
-void main(void) {
-  printf("Hello World\n");
-  printf("Goodbye World\n");
+int countNumbersWithUniqueDigits(int arr[], int n) {
+  // todo
+}
+
+int main() {
+  int arr[] = {123, 456, 789, 121, 345};
+  int n = sizeof(arr);
+  int count = countNumbersWithUniqueDigits(arr, n);
+  printf("Количество чисел с уникальными цифрами: %dn", count);
+  return 0;
 }
