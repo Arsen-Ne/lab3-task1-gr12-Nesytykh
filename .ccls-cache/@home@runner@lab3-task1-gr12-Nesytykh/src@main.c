c988155a10d6326d89150bfb32f4832bddd43155
Arsen-Ne <90570235+Arsen-Ne@users.noreply.github.com>
2023-05-28 17:44:03 +0000

    var 13 countNumbersWithUniqueDigits create

--- .ccls-cache/@home@runner@lab3-task1-gr12-Nesytykh/src@main.c
+++ .ccls-cache/@home@runner@lab3-task1-gr12-Nesytykh/src@main.c
@@ -1,14 +1,25 @@
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+bool hasUniqueDigits(int num) {
+    //todo
+}
 
 int countNumbersWithUniqueDigits(int arr[], int n) {
-  // todo
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (hasUniqueDigits(arr[i])) {
+            count++;
+        }
+    }
+    return count;
 }
 
 int main() {
-  int arr[] = {123, 456, 789, 121, 345};
-  int n = sizeof(arr);
-  int count = countNumbersWithUniqueDigits(arr, n);
-  printf("Количество чисел с уникальными цифрами: %dn", count);
-  return 0;
+    int arr[] = { 123, 456, 789, 121, 345 };
+    int n = sizeof(arr);
+    int count = countNumbersWithUniqueDigits(arr, n);
+    printf("Количество чисел с уникальными цифрами: %dn", count);
+    return 0;
 }
+
